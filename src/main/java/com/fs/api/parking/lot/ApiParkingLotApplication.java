9a538843e3b0eb067de89182a08f77de7856a060
fsirajli <Fedos6700411>
2021-09-11 16:54:40 +0400

    Initial commit

---
+++ src/main/java/com/fs/api/parking/lot/ApiParkingLotApplication.java
@@ -0,0 +1,12 @@
+package com.fs.api.parking.lot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParkingLotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiParkingLotApplication.class, args);
+    }
+}
