ef43da93d21f8be049f41d8cdcaf9a704ad29c20
fsirajli <Fedos6700411>
2021-09-12 16:58:10 +0400

    PL-2 : running app liquibase and h2

---
+++ src/main/java/com/fs/api/parking/lot/rest/HealthController.java
@@ -0,0 +1,19 @@
+package com.fs.api.parking.lot.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping(value = "/health")
+    public ResponseEntity health() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/readiness")
+    public ResponseEntity readiness() {
+        return ResponseEntity.ok().build();
+    }
+}
