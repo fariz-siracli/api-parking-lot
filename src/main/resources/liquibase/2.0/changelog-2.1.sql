4aea4ef3f8a9dd066c9714ce2826b7ffa8f121d6
fsirajli <Fedos6700411>
2021-09-14 10:15:42 +0400

    PL-4 : construct tables via liquibase add some data

---
+++ src/main/resources/liquibase/2.0/changelog-2.1.sql
@@ -0,0 +1,68 @@
+insert into parking values(1, 'Baku Parking', 50, 5,'2021-09-10');
+
+insert into floor values(1, 'Ground floor', 0, 1, 100, 4, 10,'AVAILABLE','','2021-09-10');
+insert into floor values(2, 'First floor', 1, 1, 90, 3, 10,'FULL','','2021-09-10');
+insert into floor values(3, 'Second floor', 2, 1, 80, 3, 10,'UNAVAILABLE','Accident happened','2021-09-10');
+insert into floor values(4, 'Third floor', 3, 1, 70, 3, 10,'AVAILABLE','','2021-09-10');
+insert into floor values(5, 'Fourth floor', 4, 1, 60, 3, 10,'AVAILABLE','','2021-09-10');
+
+insert into gate values (1, 1, 1, 'Old city entry', 'ENTRY', 'AVAILABLE', '2021-08-09');
+insert into gate values (2, 4, 1, 'Nizami entry', 'ENTRY', 'AVAILABLE', '2021-08-09');
+
+
+
+insert into slot values(1, 1, 1, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(2, 2, 1, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(3, 3, 1, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(4, 4, 1, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(5, 5, 1, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(6, 6, 1, 'MEDIUM', 'UNAVAILABLE', null, '2021-09-10');
+insert into slot values(7, 7, 1, 'XS', 'FREE', null, '2021-09-10');
+insert into slot values(8, 8, 1, 'SMALL', 'FREE', null, '2021-09-10');
+insert into slot values(9, 9, 1, 'LARGE', 'FREE', null, '2021-09-10');
+insert into slot values(10,10,1, 'XL', 'FREE', null, '2021-09-10');
+
+
+insert into slot values(11, 1, 2, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(12, 2, 2, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(13, 3, 2, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(14, 4, 2, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(15, 5, 2, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(16, 6, 2, 'XS', 'UNAVAILABLE', null, '2021-09-10');
+insert into slot values(17, 7, 2, 'XS', 'FREE', null, '2021-09-10');
+insert into slot values(18, 8, 2, 'SMALL', 'FREE', null, '2021-09-10');
+insert into slot values(19, 9, 2, 'SMALL', 'FREE', null, '2021-09-10');
+insert into slot values(20,10, 2, 'LARGE', 'FREE', null, '2021-09-10');
+
+insert into slot values(21, 1, 3, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(22, 2, 3, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(23, 3, 3, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(24, 4, 3, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(25, 5, 3, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(26, 6, 3, 'XS', 'UNAVAILABLE', null, '2021-09-10');
+insert into slot values(27, 7, 3, 'XS', 'FREE', null, '2021-09-10');
+insert into slot values(28, 8, 3, 'SMALL', 'FREE', null, '2021-09-10');
+insert into slot values(29, 9, 3, 'SMALL', 'FREE', null, '2021-09-10');
+insert into slot values(30,10, 3, 'LARGE', 'FREE', null, '2021-09-10');
+
+insert into slot values(31, 1, 4, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(32, 2, 4, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(33, 3, 4, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(34, 4, 4, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(35, 5, 4, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(36, 6, 4, 'XS', 'UNAVAILABLE', null, '2021-09-10');
+insert into slot values(37, 7, 4, 'XS', 'FREE', null, '2021-09-10');
+insert into slot values(38, 8, 4, 'SMALL', 'FREE', null, '2021-09-10');
+insert into slot values(39, 9, 4, 'SMALL', 'FREE', null, '2021-09-10');
+insert into slot values(40,10, 4, 'LARGE', 'FREE', null, '2021-09-10');
+
+insert into slot values(41, 1, 5, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(42, 2, 5, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(43, 3, 5, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(44, 4, 5, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(45, 5, 5, 'MEDIUM', 'FREE', null, '2021-09-10');
+insert into slot values(46, 6, 5, 'XS', 'UNAVAILABLE', null, '2021-09-10');
+insert into slot values(47, 7, 5, 'XS', 'FREE', null, '2021-09-10');
+insert into slot values(48, 8, 5, 'SMALL', 'FREE', null, '2021-09-10');
+insert into slot values(49, 9, 5, 'SMALL', 'FREE', null, '2021-09-10');
+insert into slot values(50,10, 5, 'LARGE', 'FREE', null, '2021-09-10');
