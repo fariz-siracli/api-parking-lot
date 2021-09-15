4764cc55d3a10f6f55f232233d2e51e16af0e157
fsirajli <Fedos6700411>
2021-09-15 10:16:49 +0400

    PL-28 : minor improvements

--- src/main/java/com/fs/api/parking/lot/model/GateDto.java
+++ src/main/java/com/fs/api/parking/lot/model/GateDto.java
@@ -1,7 +1,5 @@
 package com.fs.api.parking.lot.model;
 
-import com.fs.api.parking.lot.model.enums.GateState;
-import com.fs.api.parking.lot.model.enums.GateType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,10 +15,4 @@ public class GateDto {
 
     @NotNull
     private Long id;
-
-    private String name;
-
-    private GateType type;
-
-    private GateState state;
 }
