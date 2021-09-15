e91c101ed89ba32c3c79e8c88db6ca0879f50f02
fsirajli <Fedos6700411>
2021-09-15 12:13:36 +0400

    PL-30 : fix checkstyle

--- src/main/java/com/fs/api/parking/lot/service/impl/GateServiceImpl.java
+++ src/main/java/com/fs/api/parking/lot/service/impl/GateServiceImpl.java
@@ -1,9 +1,9 @@
 package com.fs.api.parking.lot.service.impl;
 
-import com.fs.api.parking.lot.logger.DPLogger;
 import com.fs.api.parking.lot.dao.GateRepository;
 import com.fs.api.parking.lot.dao.model.Gate;
 import com.fs.api.parking.lot.exception.DPException;
+import com.fs.api.parking.lot.logger.DPLogger;
 import com.fs.api.parking.lot.model.GateDto;
 import com.fs.api.parking.lot.service.GateService;
 import org.springframework.stereotype.Service;
@@ -25,7 +25,7 @@ public class GateServiceImpl implements GateService {
 
         var gateEntityOpt = gateRepository.findById(dto.getId());
 
-        return gateEntityOpt.orElseThrow(() ->{
+        return gateEntityOpt.orElseThrow(() -> {
             logger.error("LogEvent.findGateEntity.start : gate {}", dto.getId());
             throw new DPException("exception.parking-lot.gate-not-found", "Requested Gate not found");
         });
